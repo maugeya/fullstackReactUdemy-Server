7dccdd8359fbcaa584998b008b86c35e0c12296c
Angela Maugey <a.maugey@gmail.com>
2017-09-08 13:22:23 +0100

    adding passprtjs and google oauth keys and secret

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hey: 'Beyonce' });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
