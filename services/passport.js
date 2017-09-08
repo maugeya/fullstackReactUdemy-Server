d16e2094377034563aa3485673508668d0b37eb8
Angela Maugey <a.maugey@gmail.com>
2017-09-08 16:46:22 +0100

    user only being stored once in the database when using google oauth. adding promises to passport service to get check for user information

---
+++ services/passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys.js');
+
+const User = mongoose.model('users');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          console.log('user exists!');
+          //we aleady have a record with the given prfile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a user record with this ID, make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
