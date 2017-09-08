3cd307b284c20dc6004ddabdfaea889bd125982b
Angela Maugey <a.maugey@gmail.com>
2017-09-08 14:44:33 +0100

    passportjs use complete. Able to get code and access token from google.

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ const keys = require('./config/keys.js');
 const app = express();
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, (accessToken) => {
-    console.log(accessToken);
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
   })
 );
 
